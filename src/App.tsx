86f6d97a6e7305f53ef76762d82c135353eb018c
Seth Burkey <sburkey@cedarville.edu>
2024-01-20 11:12:43 -0500

    Started UI development

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { Box, Card } from '@mui/material';
+import GaugeChart from 'react-gauge-chart';
+import styleGauge from './styleGauge';
+
+
 function App() {
   return (
-    <p>Hello world!</p>
+    <Box>
+      <Box sx={{display: 'flex', flexDirection: 'row'}}>
+        <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}> 
+          <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={0.56} />
+          <p> Wheel Speed </p>
+        </Card>
+        <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={0.56} />
+          <p> Wheel Speed </p>
+        </Card>
+      </Box>
+      
+      <p>Hello world!</p>
+    </Box>
   );
 }
 
