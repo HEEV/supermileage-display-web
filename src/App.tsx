037615c50f0fb61530bae1034b6f012fe28a1ec7
Zaine F <48459250+ZGeek03@users.noreply.github.com>
2024-01-27 12:14:54 -0500

    Started work on driver display UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 export type DataEntry = {
   time: Date;
   velocity: number;
@@ -14,13 +15,14 @@ export type AppState = {
 }
 
 import { Box, Card } from '@mui/material';
-import GaugeChart from 'react-gauge-chart';
 import { Component } from 'react';
-import { Socket/*, io */} from 'socket.io-client';
+import { Socket, io } from 'socket.io-client';
+import ReactSpeedometer from 'react-d3-speedometer';
+import { Chart } from 'react-google-charts';
 
 export default class App extends Component<Record<string, string>, AppState> {
   private _socket?: Socket;
-
+  
   constructor(props: Record<string, string>) {
     super(props);
 
@@ -40,53 +42,97 @@ export default class App extends Component<Record<string, string>, AppState> {
   }
 
   componentDidMount(): void {
-    return;
-    //   this._socket = io('http://localhost:38392', {
-    //     autoConnect: false
-    //   });
-    //   this._socket.on('new_data', data => {
-    //     this.setState({
-    //       history: [data, ...this.state.history]
-    //     });
-    //   });
+    this._socket = io('https://judas.arkinsolomon.net', {
+      autoConnect: false
+    });
+    this._socket.on('new_data', data => {
+      this.setState({
+        history: [data, ...this.state.history]
+      });
+    });
 
-    //   this._socket.on('new_race_created', name => {
-    //     this.setState({
-    //       history: [],
-    //       currentRaceName: name
-    //     });
-    //   });
+    this._socket.on('new_race_created', name => {
+      this.setState({
+        history: [],
+        currentRaceName: name
+      });
+    });
 
-    //   this._socket.on('current_race', name => {
-    //     this.setState({
-    //       currentRaceName: name
-    //     });
-    //   });
-    //   this._socket.connect();
+    this._socket.on('current_race', name => {
+      this.setState({
+        currentRaceName: name
+      });
+    });
+    this._socket.connect();
   }
 
-  // componentWillUnmount(): void {
-  //   this._socket?.disconnect();
-  // }
+  componentWillUnmount(): void {
+    this._socket?.disconnect();
+  }
 
   render() {
     return (
       <>
-        <Box>
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={ this.state.history[0].velocity / 50 } />
-              <p> Wheel Speed </p>
+        <h1>Race: {this.state.currentRaceName}</h1>
+        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', margin: '10px' }}>
+            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px'}}>
+              <Chart
+                chartType='Gauge'
+                height='400px'
+                loader={<div>Loading...</div>}
+                data={[
+                  ['Label', 'Value'],
+                  ['Speed (MPH)', this.state.history[0].velocity]
+                ]}
+                options={{
+                  minorTicks: 5,
+                  max: 35,
+                }}
+              />
             </Card>
-            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={ this.state.history[0].engineTemp / 200 } />
-              <p> Engine Temp </p>
+            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '20px' }}>
+              <ReactSpeedometer 
+                maxValue={70}
+                minValue={-70} 
+                value={this.state.history[0].tilt} 
+                segments={7}
+                maxSegmentLabels={7} 
+                currentValueText='${value} Degrees' 
+                segmentColors={['red', 'red', 'yellow', 'green', 'yellow', 'red', 'red']} 
+              />
+            </Card>
+            <Card>
+              <Chart
+                chartType='Gauge'
+                height='400px'
+                loader={<div>Loading...</div>}
+                data={[
+                  ['Label', 'Value'],
+                  ['Temp (F)', this.state.history[0].engineTemp]
+                ]}
+                options={{
+                  redFrom: 180,
+                  redTo: 200,
+                  yellowFrom: 155,
+                  yellowTo: 180,
+                  minorTicks: 5,
+                  max: 200,
+                }}
+              />
+            </Card>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', margin: '10px' }}>
+            <Card>
+              <h3>
+                Track Map
+              </h3>
+
             </Card>
           </Box>
         </Box>
 
-        <h1>{this.state.currentRaceName}</h1>
-        <p>{JSON.stringify(this.state.history, null, 4)}</p>
+        
       </>
     );
   }
--- src/styleGauge.tsx
+++ src/styleGauge.tsx
@@ -2,10 +2,10 @@ import { Card } from '@mui/material';
 import GaugeChart from 'react-gauge-chart';
 
 export default function styleGauge() {
-    return (
-        <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={0.56} />
-            <p> Wheel Speed </p>
-        </Card>
-    )
+  return (
+    <Card sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <GaugeChart id="gauge-chart1" nrOfLevels={40} percent={0.56} />
+      <p> Wheel Speed </p>
+    </Card>
+  );
 }
