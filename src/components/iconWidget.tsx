80fa921bd8cf085425b860984b7325fd7ad92eba
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-11-08 13:23:21 -0500

    simplified indicatorIcon component and added css class to styles.css

--- src/components/iconWidget.tsx
+++ src/components/iconWidget.tsx
@@ -9,15 +9,12 @@ export default function IndicatorIcon(props: {
   const { on, text, Icon, iconWidth } = props;
   const bgColor = Icon ? 'transparent' : (on ? 'var(--color-icon-on)' : 'var(--color-icon-off)');
   const imgColor = Icon ? (on ? 'var(--color-icon-on)' : 'var(--color-icon-off)') : 'var(--color-icon-text)';
-
+  const spanContent = text || (Icon && <Icon width={30} height={30} fill="currentColor" />);
+  
   return (
     <div
+      className={'indicator-icon'}
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: '4px',
-        padding: '4px 6px',
         width: iconWidth, 
         backgroundColor: bgColor,
       }}
@@ -25,10 +22,9 @@ export default function IndicatorIcon(props: {
       <span
         style={{
           color: imgColor,
-          whiteSpace: 'nowrap',
         }}
       >
-        {text ? <strong>{text}</strong> : Icon ? (<Icon width={30} height={30} fill="currentColor" />) : null}
+        {spanContent}
       </span>
     </div>
   );
