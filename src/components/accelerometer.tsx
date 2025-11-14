b70a940f11c24501cf450d218d5cb955ec5edfab
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-11-13 22:28:13 -0500

    made a basic bar, but still needs work

--- src/components/accelerometer.tsx
+++ src/components/accelerometer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ReactComponent as CarIcon } from '../styles/icons/car-solid-full.svg';
 
+function renderCarIcon(angle = 45, size = 30) {
+  return <CarIcon width={size} height={size} fill="currentColor" style={{ transform: `rotate(${angle}deg)`, transformOrigin: 'center' }} />;
+}
+
 export default function Accelerometer(props: {
   state: 'good' | 'bad' | 'warn';
   display?: 'icon' | 'bar';
 }) {
   const { state, display = 'icon' } = props;
-  const bgColor = state === 'good' ? 'var(--color-accel-good)' : (state === 'bad' ? 'var(--color-accel-bad)' : 'var(--color-accel-caut)');
-  const carIcon = (<CarIcon width={30} height={30} fill="currentColor" style={{ transform: 'rotate(45deg)', transformOrigin: 'center' }} />);
+  const bgColor = state === 'good' ? 'var(--color-accel-good)' : 
+    (state === 'bad' ? 'var(--color-accel-bad)' : 'var(--color-accel-caut)');
   
   return (
     display === 'icon' ? (
@@ -22,14 +26,14 @@ export default function Accelerometer(props: {
             color: bgColor,
           }}
         >
-          {carIcon}!
+          {renderCarIcon()}!
         </span>
       </div>
     ) : (
       <div
         className={'accelerometer-bar'}
       >
-        <CarIcon width={30} height={30} fill='black' style={{ transform: 'rotate(0deg)', transformOrigin: 'center' }} />
+        {renderCarIcon(0)}
       </div>
     )
   );
